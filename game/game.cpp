26a4f83cfa17eb741f102cf8e38a5752b747e586
blackkitten1 <blackkitten010@gmail.com>
2020-09-05 20:37:19 +0700

    .

--- game/game.cpp
+++ game/game.cpp
@@ -1,47 +1,75 @@
-#include <SFML/Graphics.hpp>
 #include <iostream>
-int main()
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+#include <math.h>
+#include <cstdlib>
 
+using namespace sf;
+
+class Player
+{
+public:
+	Sprite shape;
+	Texture *texture;
+	int HP;
+	int HPMax;
+
+	Player(Texture *texture)
+	{
+		this->HPMax = 10;
+		this->HP = this->HPMax;
+
+		this->texture = texture;
+		this->shape.setTexture(*texture);
+	}
+	~Player(){}
+};
+
+class Enemy
 {
 
-	sf::RenderWindow window(sf::VideoMode(1080, 720), "Thunder Stike");
-	////// Texture
-	sf::Texture playerTexture;
-	if (!playerTexture.loadFromFile("img/spaceship1.png"))
-	{
-		std::cout << "Load failed" << std::endl;
-	}
+};
 
-	sf::Sprite shapeSprite;
-	shapeSprite.setTexture(playerTexture);
-	shapeSprite.setTextureRect(sf::IntRect(0, 0, 70, 70));
+class Bullet
+{
+
+};
+
+int main()
+{
+	srand(time(NULL));
+
+	RenderWindow window(sf::VideoMode(1080, 720), "Thunder Stike",Style::Default);
+	window.setFramerateLimit(60);
+
+	Texture playerTex;
+	playerTex.loadFromFile("img/spaceship.png");
+
+	Texture enemyTex;
+	playerTex.loadFromFile("img/rock.png");
+
+	Texture bulletTex;
+	playerTex.loadFromFile("img/bullet.png");
+
+	Player player(&playerTex);
 
 	while (window.isOpen())
 	{
-		window.draw(shapeSprite);
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+
+		window.draw(player.shape);
 		
 		window.display();
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-		{
-			shapeSprite.move(.1f, 0.f);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-		{
-			shapeSprite.move(-.1f, 0.f);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-		{
-			shapeSprite.move(0.f, -.1f);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-		{
-			shapeSprite.move(0.f, .1f);
-		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-		{
-			window.close();
-		}
-		window.clear();
 	}
 	return 0;
 }
